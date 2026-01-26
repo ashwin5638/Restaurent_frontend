8ec82b1285c82a47c3ce84cf204f4ec13bcde468
Ashwin <138807279+ashwin5638@users.noreply.github.com>
2026-01-26 10:24:36 +0530

    Update registration API endpoint to production URL

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -18,7 +18,7 @@ const handleRegister = async (e) => {
   e.preventDefault()
 
   try {
-    const res = await fetch('http://localhost:5000/register', {
+    const res = await fetch('https://restaurent-backend-1-3fc8.onrender.com/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
