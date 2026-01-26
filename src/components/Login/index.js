6e74ed6b8fc8370691d3761da6193bf477acf2ff
Ashwin <138807279+ashwin5638@users.noreply.github.com>
2026-01-26 10:24:09 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -24,7 +24,7 @@
 
     try{
         
-        const res = await fetch('http://localhost:5000/login', {
+        const res = await fetch('https://restaurent-backend-1-3fc8.onrender.com/login', {
             method : 'POST',
             headers : {'Content-Type': 'application/json'},
             body : JSON.stringify({
